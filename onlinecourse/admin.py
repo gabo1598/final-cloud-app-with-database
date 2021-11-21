518244a8ef954dcb7d6fe4c80732b77897d95cd1
gabo1598 <palmag@uabc.edu.mx>
2021-11-21 07:00:20 +0000

    changes made from theia lab environment

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+
+from .models import Course, Lesson, Instructor, Learner , Choice, Question, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
@@ -8,7 +9,12 @@ from .models import Course, Lesson, Instructor, Learner
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
-
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 1
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 1
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -16,6 +22,15 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
+    
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('get_question', 'choice_text', 'is_correct')
+    list_filter = ['is_correct']
+    search_fields = ['choice_text']
+    def get_question(self, obj):
+        return obj.question.question_text
+    get_question.short_description = 'Question'
+    get_question.admin_order_field = 'question_text'
 
 
 class LessonAdmin(admin.ModelAdmin):
@@ -28,3 +43,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question)
+admin.site.register(Choice)
